cdcb5d1b70c4f7e970975b271e78ddc3f89c1c86
Fadzi Ushewokunze <fushewokunze@pivotal.io>
2017-03-31 14:29:08 -0400

    Update rds.tf

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -18,3 +18,20 @@ resource "aws_db_instance" "rds" {
 
   count = "${var.rds_instance_count}"
 }
+
+resource "aws_db_instance" "aws_service_broker" {
+  allocated_storage      = 100
+  instance_class         = "${var.rds_instance_class}"
+  engine                 = "mysql"
+  engine_version         = "5.6.22"
+  identifier             = "${var.env_name}"
+  username               = "${var.rds_db_username}"
+  password               = "${random_id.rds_password.b64}"
+  db_subnet_group_name   = "${aws_db_subnet_group.rds_subnet_group.name}"
+  publicly_accessible    = true
+  vpc_security_group_ids = ["${aws_security_group.mysql_security_group.id}"]
+  iops                   = 1000
+  multi_az               = true
+
+  count = "${var.rds_instance_count}"
+}
